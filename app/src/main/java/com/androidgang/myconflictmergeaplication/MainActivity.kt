5130800d10a4f5fd8713258195b6f567504d612f
AndrewBlack-hub <crys2014@mail.ru>
2021-04-08 02:13:33 +0700

    do something

--- app/src/main/java/com/androidgang/myconflictmergeaplication/MainActivity.kt
+++ app/src/main/java/com/androidgang/myconflictmergeaplication/MainActivity.kt
@@ -7,5 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val name: String = "Andrew"
+        val surname: String = "Doe"
+    }
+
+    private fun getData(): String {
+        return "Data"
     }
 }
